9fd282a0094b6cbe0321ee529d7bdfa97a62e5e8
JP <80105602+JP3015@users.noreply.github.com>
2022-12-30 14:14:11 -0300

    Arquivos

---
+++ index.js
@@ -0,0 +1,41 @@
+let computerNumber = Math.floor(Math.random() * 100 + 1)
+let userNumbers = []
+let attempts = 0
+
+
+console.log(computerNumber)
+
+function compareNumbers() {
+    const userNumber = Number(document.getElementById('inputBox').value)
+    
+    if(userNumber > 100){
+        document.getElementById('textOutput').innerHTML = 'Esse número é maior que 100'
+        document.getElementById('inputBox').value = ''
+    }
+
+    else if(attempts < 10){
+        if (userNumber == computerNumber){
+            document.getElementById('textOutput').innerHTML = 'Parabens!!!'
+            document.getElementById('inputBox').setAttribute('Readonly','Readonly')
+        }else if (userNumber > computerNumber){
+            document.getElementById('textOutput').innerHTML = 'Número muito grande'
+            document.getElementById('inputBox').value = ''
+        }else{
+            document.getElementById('textOutput').innerHTML = 'Número muito pequeno'
+            document.getElementById('inputBox').value = ''
+        }
+        attempts += 1 
+        document.getElementById('attempts').innerHTML = attempts
+        userNumbers.push(userNumber)
+        document.getElementById('guesses').innerHTML = userNumbers
+    }else{
+        document.getElementById('textOutput').innerHTML = 'Você já fez 10 tentativas'
+        document.getElementById('inputBox').setAttribute('Readonly','Readonly')
+    }
+    
+}
+
+function newGame(){
+    window.location.reload()
+}
+
